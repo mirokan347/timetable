cb3e1a16e3333a0b7e911298a8553c345fc0428b
miro <miroslaw.kania82@gmail.com>
2022-12-18 20:10:28 +0100

    initial commit

---
+++ schedule/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+# from .views import ()
+
+app_name = 'lesson'
+
+urlpatterns = [
+    #path('', ArticleListView.as_view(), name='article-list'),
+    #path('create/', ArticleCreateView.as_view(), name='article-create'),
+    #path('<int:id>/', ArticleDetailView.as_view(), name='article-detail'),
+    #path('<int:id>/update/', ArticleUpdateView.as_view(), name='article-update'),
+    #path('<int:id>/delete/', ArticleDeleteView.as_view(), name='article-delete'),
+]
