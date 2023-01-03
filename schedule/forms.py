2fef0b09f0b8de3f9b5ae2107193549625b2174e
miro <miroslaw.kania82@gmail.com>
2023-01-03 19:28:02 +0100

    create datatime picker

--- schedule/forms.py
+++ schedule/forms.py
@@ -1,12 +1,9 @@
 from django import forms
-
+from datetimewidget.widgets import DateTimeWidget
 from .models import Lesson
 
 
 class LessonModelForm(forms.ModelForm):
-    start_time = forms.DateTimeField()
-    end_time = forms.DateTimeField()
-
     class Meta:
         model = Lesson
         fields = [
@@ -18,3 +15,9 @@ class LessonModelForm(forms.ModelForm):
             'class_group',
             'teacher',
         ]
+
+        widgets = {
+            # Use localization and bootstrap 3
+            'start_time': DateTimeWidget(attrs={'id': "yourdatetimeid"}, usel10n=True, bootstrap_version=3),
+            'end_time': DateTimeWidget(attrs={'id': "yourdatetimeid"}, usel10n=True, bootstrap_version=3)
+        }
--- timetable/settings.py
+++ timetable/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'datetimewidget',
     # third party
 
     # own
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware'
 ]
 
 ROOT_URLCONF = 'timetable.urls'
