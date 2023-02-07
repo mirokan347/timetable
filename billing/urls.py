9137d68fa17ddce81b031c19373fd6f4a202a49a
miro <miroslaw.kania82@gmail.com>
2023-02-07 21:58:34 +0100

    create billing

---
+++ billing/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import bill, bill_detail
+
+app_name = 'billing'
+
+urlpatterns = [
+    path('student/<int:student_id>/bill/', bill, name='bill'),
+    path('bill/<int:pk>/', bill_detail, name='bill-detail'),
+]
--- billing/views.py
+++ billing/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from users.models import Student
+from .models import Billing
+
+
+def bill(request, student_id):
+    student = get_object_or_404(Student, id=student_id)
+    billing = Billing.objects.filter(student=student)
+    return render(request, 'bill.html', {'student': student, 'payments': billing})
+
+@login_required
+def bill_detail(request, pk):
+    bill = get_object_or_404(Billing, pk=pk)
+    if bill.student.user != request.user:
+        # only display the bill if the associated student matches the currently logged in user
+        return render(request, 'error.html', {'message': 'Access Denied'})
+    return render(request, 'bill_detail.html', {'bill': bill})
